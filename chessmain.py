9b6380be03163dc039e10607fdb9854c5ba9f5f8
Ishaan Shukla <ishaanshukla10@gmail.com>
2025-04-20 19:03:39 +0530

    Added random move maker to play against bot

--- chessmain.py
+++ chessmain.py
@@ -1,5 +1,6 @@
 import pygame as p
 import chessengine
+import smartmovefinder
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -29,14 +30,16 @@ def main():
     sqSelected = ()
     playerClicks = []
     gameOver = False
-
+    playerOne = True
+    playerTwo = False
 
     while running:
+        humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                if not gameOver:
+                if not gameOver and humanTurn:
                     location = p.mouse.get_pos()
                     col = location[0]//SQ_SIZE
                     row = location[1]//SQ_SIZE
@@ -70,6 +73,13 @@ def main():
                     playerClicks = []
                     moveMade = False
                     animate = False
+
+        if not gameOver and not humanTurn:
+            AIMove = smartmovefinder.findRandomMove(validMoves)
+            gs.makeMove(AIMove)
+            moveMade = True
+            animate = True
+
 
         if moveMade:
             if animate:
---
+++ smartmovefinder.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+
+def findRandomMove(validMoves):
+    return validMoves[random.randint(0, len(validMoves)-1)]
+
+
+def findBestMove():
+    pass
