49f20045153606b3c789a55e04e55d6e7cb15008
Ishaan Shukla <ishaanshukla10@gmail.com>
2025-04-21 00:49:46 +0530

    Bug fix

--- chessengine.py
+++ chessengine.py
@@ -439,7 +439,7 @@ class GameState():
     
     def getQueenSideCastleMoves(self, r, c, moves):
         if self.board[r][c-1] == '--' and self.board[r][c-2] == '--' and self.board[r][c-3] == '--':
-            if not self.squareUnderAttack(r, c-1) and not self.squareUnderAttack(r, c-2) and not self.squareUnderAttack(r, c-3):
+            if not self.squareUnderAttack(r, c-1) and not self.squareUnderAttack(r, c-2):
                 moves.append(Move((r, c), (r, c-2), self.board, isCastleMove=True))
 
 
