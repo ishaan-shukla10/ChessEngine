ee0cf42ab5382c66c5954925a6a8ac43209d31ee
Ishaan Shukla <ishaanshukla10@gmail.com>
2025-04-21 23:34:32 +0530

    Minor animation bug fix

--- chessmain.py
+++ chessmain.py
@@ -105,7 +105,7 @@ def main():
                                 if move == validMoves[i]:
                                     gs.makeMove(validMoves[i])
                                     moveMade = True
-                                    animate = True
+                                    animate = False
                                     sqSelected = ()
                                     playerClicks = []
                                     break
